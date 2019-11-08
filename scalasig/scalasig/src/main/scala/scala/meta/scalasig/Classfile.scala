58f7f74ee0c1d8867255547cc206e8e2caac5ef8
Win Wang <winw@twitter.com>
2019-11-08 14:56:52 -0800

    fixup! wip: .sig -> .class

--- scalasig/scalasig/src/main/scala/scala/meta/scalasig/Classfile.scala
+++ scalasig/scalasig/src/main/scala/scala/meta/scalasig/Classfile.scala
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE.md).
 package scala.meta.scalasig
 
+import java.io.ByteArrayOutputStream
 import scala.meta.internal.scalasig._
 
 case class Classfile(name: String, source: String, payload: Payload) {
@@ -18,8 +19,30 @@ case class Classfile(name: String, source: String, payload: Payload) {
 object Classfile {
   def fromBinary(binary: Binary): ClassfileResult = {
     try {
-      val classfile = ClassfileCodec.fromBinary(binary)
-      ParsedClassfile(binary, classfile)
+      binary match {
+        case b@PathBinary(path) if path.toString.endsWith(".sig") =>
+          val pathStr = b.path.toString
+          val stream = b.openStream()
+          val bytes = try {
+            val baos = new ByteArrayOutputStream()
+            val buf = new Array[Byte](1024)
+            var len = stream.read(buf)
+            while (len != -1) {
+              baos.write(buf, 0, len)
+              len = stream.read(buf)
+            }
+            baos.toByteArray
+          } finally stream.close()
+
+          ParsedClassfile(binary, Classfile(
+            pathStr.stripSuffix(".sig"),
+            pathStr,
+            ScalaPayload(bytes)
+          ))
+        case _ =>
+          val classfile = ClassfileCodec.fromBinary(binary)
+          ParsedClassfile(binary, classfile)
+      }
     } catch {
       case ex: Throwable =>
         FailedClassfile(binary, ClassfileReadException(binary, ex))
