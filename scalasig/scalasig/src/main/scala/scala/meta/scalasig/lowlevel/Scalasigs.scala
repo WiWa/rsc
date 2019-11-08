ee310cd0e6b2cd1b5b467d81b348a5c89efe732c
Win Wang <winw@twitter.com>
2019-11-08 10:23:35 -0800

    wip: .sig integration should happen lower in stack

--- scalasig/scalasig/src/main/scala/scala/meta/scalasig/lowlevel/Scalasigs.scala
+++ scalasig/scalasig/src/main/scala/scala/meta/scalasig/lowlevel/Scalasigs.scala
@@ -2,7 +2,9 @@
 // Licensed under the Apache License, Version 2.0 (see LICENSE.md).
 package scala.meta.scalasig.lowlevel
 
+import java.io.{BufferedInputStream, ByteArrayOutputStream, InputStream}
 import java.nio.file._
+import scala.meta.internal.scalasig.ScalasigCodec
 import scala.meta.scalasig._
 
 object Scalasigs {
@@ -11,6 +13,30 @@ object Scalasigs {
   }
 
   def apply(path: Path)(fn: ScalasigResult => Unit): Unit = {
-    Binaries(path)(binary => fn(Scalasig.fromBinary(binary)))
+    if (path.toString.endsWith(".sig")) {
+      val bytes = {
+        val stream: InputStream = new BufferedInputStream(Files.newInputStream(path))
+        try {
+          val baos = new ByteArrayOutputStream()
+          val buf = new Array[Byte](1024)
+          var len = stream.read(buf)
+          while (len != -1) {
+            baos.write(buf, 0, len)
+            len = stream.read(buf)
+          }
+          baos.toByteArray
+        }
+        finally stream.close()
+      }
+      val ss = ScalasigCodec.fromBytes(path.toString, "", bytes)
+      val pss = ParsedScalasig(
+        PathBinary(path),
+        Classfile(path.toString, path.toString, ScalaPayload(bytes)),
+        ss
+      )
+      fn(pss)
+    } else {
+      Binaries(path)(binary => fn(Scalasig.fromBinary(binary)))
+    }
   }
 }
