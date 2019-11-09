0009624d35c0c0d125fecc01a8a37263bf5198b2
Win Wang <winw@twitter.com>
2019-11-08 16:24:36 -0800

    lots of wip, SigToClass for sig jars work, more refactoring

--- scalasig/scalasig/src/main/scala/scala/meta/scalasig/Binary.scala
+++ scalasig/scalasig/src/main/scala/scala/meta/scalasig/Binary.scala
@@ -5,10 +5,14 @@ package scala.meta.scalasig
 import java.io._
 import java.net._
 import java.nio.file._
+import java.util.jar.JarFile
+import java.util.zip.ZipEntry
 import scala.language.implicitConversions
 
 sealed trait Binary {
   def openStream(): InputStream
+
+  def isSig: Boolean
 }
 
 object Binary {
@@ -19,20 +23,30 @@ object Binary {
 
 case object NoBinary extends Binary {
   def openStream(): InputStream = throw new UnsupportedOperationException()
+  def isSig: Boolean = false
   override def toString: String = "<none>"
 }
 
 case class UriBinary(uri: URI) extends Binary {
   def openStream(): InputStream = uri.toURL.openStream()
+  def isSig: Boolean = uri.toString.endsWith(".sig")
   override def toString: String = uri.toURL.toString
+}
+object UriBinary {
+  def apply(jarfile: JarFile, entry: ZipEntry): UriBinary = {
+    val uri = new URL("jar:file:" + jarfile.getName + "!/" + entry.getName).toURI
+    new UriBinary(uri)
+  }
 }
 
 case class PathBinary(path: Path) extends Binary {
   def openStream(): InputStream = Files.newInputStream(path)
+  def isSig: Boolean = path.toString.endsWith(".sig")
   override def toString: String = path.toString
 }
 
 case class BytesBinary(label: String, bytes: Array[Byte]) extends Binary {
   def openStream(): InputStream = new ByteArrayInputStream(bytes)
+  def isSig: Boolean = false
   override def toString: String = label
 }
