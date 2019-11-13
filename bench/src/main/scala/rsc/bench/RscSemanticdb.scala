a6dfe85cbd2707a8cf91909b0caa2e3617bcd45c
Win Wang <winw@twitter.com>
2019-11-13 11:43:36 -0800

    benchSemanticdb should write artifact

--- bench/src/main/scala/rsc/bench/RscSemanticdb.scala
+++ bench/src/main/scala/rsc/bench/RscSemanticdb.scala
@@ -14,6 +14,6 @@ import org.openjdk.jmh.annotations.Mode._
 class RscSemanticdb extends RscBenchmark {
   @Benchmark
   def run(bs: BenchmarkState): Unit = {
-    benchCompiler("-Ystop-after:semanticdb", "-cp", bs.rscDeps, bs.files)
+    benchCompiler("-Ystop-after:semanticdb", "-artifacts", "semanticdb", "-cp", bs.rscDeps, bs.files)
   }
 }
